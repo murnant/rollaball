301379f3e912c456ec22521b8e7973b3bfafea03
murnant <74027673+murnant@users.noreply.github.com>
2021-08-28 11:00:05 -0500

    you win

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -2,18 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class playerController : MonoBehaviour
 {
     public float Speed = 0;
-
+    public TextMeshProUGUI countText;
+    public GameObject WinText;
+    private int count;
     private Rigidbody rb;
     private float movementX;
     private float movementY;
+    private int NumberOfPickUps;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
+        WinText.SetActive(false);
+        GameObject[] thingyToFind = GameObject.FindGameObjectsWithTag ("PickUp");
+        NumberOfPickUps = thingyToFind.Length;
+
     }
 
 
@@ -26,9 +37,30 @@ public class playerController : MonoBehaviour
     }
 
 
+    void SetCountText()
+    {
+        countText.text = "Conunt; " + count.ToString();
+        if (count >= NumberOfPickUps)
+        {
+            WinText.SetActive(true);
+        }
+    }
+
+
     void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX,0.0f,movementY);
         rb.AddForce(movement * Speed);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count +1;
+            SetCountText();
+        }
     }
 }
